325cc2daaa9d3447c3ab00462cbf8fb51003a704
carlozaul25 <carloz18rivera@live.com>
2018-05-14 23:10:29 -0400

    wrong-ans

--- index.js
+++ index.js
@@ -4,61 +4,55 @@ var lives = 0;
 var answerArray = [];
 var found = 0;
 var img = document.getElementById('img');
+var blackThumb = document.getElementById('blackThumb');
 var win = document.getElementById('Youwin');
 var textWin = document.getElementById('textWin');
+var answer = document.getElementById("answer");
+var livesText=document.getElementById('livesText');
+var youLose = document.getElementById('youLose');
+// document.getElementById("answer").innerHTML = answerArray.join(" ");
  
 
 function startUp() {
 	randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
-
 	for (var i = 0; i < randomWord.length; i++) {
-												
-		answerArray[i]= "_"
+			answerArray[i]= "_"
 	}
-
-
-ans = answerArray.join(" ");
-document.getElementById("answer").innerHTML = ans;
-
+		ans = answerArray.join(" ");
+		answer.innerHTML = ans;
 }
 
 function showLetters() {
-
 	var letter = document.getElementById("letter").value;
 	if (letter.length > 0 ) {
 		for (var i = 0; i < randomWord.length; i++) {
 			if (randomWord[i] == letter) {
 				answerArray[i] = letter;
-				document.getElementById("answer").innerHTML = answerArray.join(" ");
+				answer.innerHTML = answerArray.join(" ");
 				found = 1
 			}
 		}
-
-		if (found === 0) {
+			if (found === 0) {
 				lives++
-			document.getElementById("lives").innerHTML = "Number of lives left out of 4: " + lives;
-
+			livesText.innerHTML = "Number of lives left out of 4: " + lives;
+			img.style.backgroundRepeat='repeat-x';
+			img.style.display='inline-block';
 		}
 			if (lives > 3 ) {				
-	
-		document.getElementById("youLose").innerHTML = "You Lose! Try again?";
+				youLose.innerHTML = "You Lose! Try again?";
 		lives = 0;
-		startUp()
-		}
-		
-		found = 0;
-
-		if (answerArray.indexOf("_") == -1) {
-			document.getElementById('textWin').innerHTML="You Win!";
-			img.style.display='inline-block';
-			textWin.style.fontSize='60px';
-			textWin.style.color='blue';
-
+	}
+			found = 0;
+			if (answerArray.indexOf("_") == -1) {
+				textWin.innerHTML="You Win!";
+				img.style.display='inline-block';
+				textWin.style.fontSize='60px';
+				textWin.style.color='blue';
 		} 
 	}
 }
 	
-	document.getElementById("answer").innerHTML = answerArray.join(" ");
+	
 
 
 	
